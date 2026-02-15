71c06d49d9a37402f84aa2e17d877e96f429b46b
Gadget <gadget@Gadgets-Mac-mini.local>
2026-02-14 21:07:16 -0500

    Convert to client-side app for GitHub Pages

--- api/leaderboard.js
+++
@@ -1,6 +0,0 @@
-const { getLeaderboard } = require('./_leaderboard');
-
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({ entries: getLeaderboard() });
-};
